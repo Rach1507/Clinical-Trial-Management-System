f8a268774a70813a4b3489a2dc827f5a1c7247eb
phanishree <56950692+phanishree@users.noreply.github.com>
2021-07-31 14:05:44 +0530

    Human subject dashboard is funtional. added modify, delete, dispplay. changed the parameters of the functions.

--- C-T-M.cpp
+++ C-T-M.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <string>
 using namespace std;
+static int dashboardFlag;
 class ctm
 {
 public:
@@ -13,16 +14,17 @@ public:
 
 int get_num_records(char filename[])
 {
-    int I = 1;
+    int I = 0;
     fstream file2;
     file2.open(filename, ios::in);
     if (!file2)
     {
-        cout << "Error in opening file";
+        cout << "Error in get num recs";
         return 0;
     }
-    while (!file2.fail()) //Unpacking record data
+    while (!file2.fail())
     {
+         //cout << "Error in get num recs in while";
         file2.getline(stdrec[I].ind, 5, '|');
         file2.getline(stdrec[I].field1, 20, '|');
         file2.getline(stdrec[I].field2, 15, '|');
@@ -33,7 +35,7 @@ int get_num_records(char filename[])
     }
     I--;
     file2.close();
-    return I - 2;
+    return I ;
 }
 
 void write_details(char filename[])
@@ -43,9 +45,10 @@ void write_details(char filename[])
     file2.open(filename, ios::in);
     if (!file2)
     {
+        cout<<"erroe in write details file";
         return;
     }
-    if (!file2.fail()) //Unpacking record data
+    if (!file2.fail()) 
     {
         file2.getline(stdrec[0].ind, 5, '|');
         file2.getline(stdrec[0].field1, 20, '|');
@@ -60,40 +63,44 @@ void write_details(char filename[])
     file2.close();
 }
 
-void add_record(string filename)
+void add_record(string filename,string indFilename)
 {
-    string index_str = filename + "-idx.txt";
+   // string index_str = filename + "-idx.txt";
 
-    filename = filename + ".txt";
+    //filename = filename + ".txt";
 
-    int n = index_str.length();
+    int n = indFilename.length();
     int n0 = filename.length();
 
     char index[n + 1];
     char record_file[n0 + 1];
 
-    strcpy(index, index_str.c_str());
+    strcpy(index, indFilename.c_str());
     strcpy(record_file, filename.c_str());
 
-    char buf1[100], buf2[20];
+    char buf1[100], buf2[20],header;
     fstream file1, file2;
     int I, cnt;
     ctm s;
 
     cnt = get_num_records(record_file);
-    // cout<<"No of recordings in file "<<cnt;
+     cout<<"No of recordings in file "<<cnt;
     file1.open(index, ios::out | ios::app);
     if (!file1)
     {
         cout << "\nError !\n";
         exit(0);
     }
-
     file2.open(record_file, ios::out | ios::app);
     if (!file2)
     {
         cout << "\nError !\n";
         exit(0);
+    }
+     if(cnt==0&&filename.compare("drug.txt")==0){
+        file2<<"ind|Drug name|Drug type|Department|Stage|Trial-Stage\n";
+    }else if(cnt==0){
+        file2<<"ind|HSI|HSN|Phone|LD|Trial-Stage\n";
     }
     int no;
     cout << "\nEnter the no. of records to be added : ";
@@ -101,20 +108,29 @@ void add_record(string filename)
     cout << "\nEnter the details :\n";
 
     write_details(record_file);
+    int j=0;
 
-    for (I = cnt; I < (cnt + no); I++)
+    for (I = cnt,j=1; I < (cnt + no); I++)
     {
+        cout<<"Enter the details of drug "<<j++;
+        write_details(record_file);
         cin >> s.field1;
         cin >> s.field2;
         cin >> s.field3;
         cin >> s.field4;
         cin >> s.field5;
 
+        if(cnt==0){
         sprintf(buf2, "%s|%d\n", s.field1, I);
         file1 << buf2;
-
         sprintf(buf1, "%d|%s|%s|%s|%s|%s\n", I, s.field1, s.field2, s.field3, s.field4, s.field5);
         file2 << buf1;
+        }else{
+        sprintf(buf2, "%s|%d\n", s.field1, I-1);
+        file1 << buf2;
+        sprintf(buf1, "%d|%s|%s|%s|%s|%s\n", I-1, s.field1, s.field2, s.field3, s.field4, s.field5);
+        file2 << buf1;
+        }
     }
     file1.close();
     file2.close();
@@ -124,35 +140,35 @@ void retrieve_details(char index_no[], char file_name[])
 {
 
     int no = get_num_records(file_name);
+    cout<<no<<" numbers";
 
     for (int i = 0; i < no; i++)
     {
-
         if (strcmp(stdrec[i].ind, index_no) == 0)
         {
             cout << "\n\n"
-                 << "Details : ";
+                 << "Details :\n";
             write_details(file_name);
-            cout << "\t" << stdrec[i].field1 << "\t" << stdrec[i].field2 << "\t" << stdrec[i].field3 << "\t" << stdrec[i].field4 << "\t" << stdrec[i].field5 << "\n";
+            cout << "\t" << stdrec[i].field1 << "\t\t" << stdrec[i].field2 << "\t\t" << stdrec[i].field3 << "\t\t" << stdrec[i].field4 << "\t\t" << stdrec[i].field5 << "\n";
             break;
         }
     }
 }
 
-void search_record(string filename)
+void search_record(string filename,string indFilename)
 {
-    string index_str = filename + "-idx.txt";
+   // string index_str = filename + "-idx.txt";
 
-    filename = filename + ".txt";
+    //filename = filename + ".txt";
 
-    int n = index_str.length();
+    int n = indFilename.length();
     int n0 = filename.length();
     // declaring character array
     char index[n + 1];
     char record_file[n0 + 1];
     // copying the contents of the
     // string to char array
-    strcpy(index, index_str.c_str());
+    strcpy(index, indFilename.c_str());
     strcpy(record_file, filename.c_str());
 
     int I, flag1;
@@ -160,9 +176,12 @@ void search_record(string filename)
     fstream file1;
 
     // cout<<index;
-
-    cout << "\nPlease enter the Name of the drug";
-    cout << " whose record is to be displayed\n";
+    if(dashboardFlag==0){
+    cout << "\nEnter the Name of the drug";
+    cout << " whose record is to be searched\n";
+    }else{
+        cout<<"\nEnter the Human Subject Id which isto be searched";
+    }
     cin >> required_name;
 
     file1.open(index, ios::in);
@@ -183,6 +202,7 @@ void search_record(string filename)
             // cout <<filename;
             retrieve_details(st_no, record_file);
             flag1 = 1;
+            cout<<"hello";
             break;
         }
     }
@@ -190,12 +210,234 @@ void search_record(string filename)
         cout << "\nRecord search failed !!\n";
     file1.close();
 }
-void delete_record()
+
+void delete_drugRec(string fileName,string indxFilename,string indx_no,string drugName)
 {
+int I=0;
+fstream file1,file2;
+char filenamechr[10],idxfilenamechr[10];
+//string filename1="drug.txt";
+
+// strcpy(idxfilenamechr,indxFilename.c_str());
+  strcpy(filenamechr, fileName.c_str());
+
+int	cnt = get_num_records(filenamechr);	
+	int flag=-1;
+	for(I=0;I<cnt;I++)     
+	{
+        string ind=stdrec[I].ind;
+        if(ind.compare(indx_no)==0)
+		{
+			flag=I;
+			break;
+		}	
+
+	}
+	if(flag==-1)              
+	{
+		cout<<"\nError !\n";
+		return;
+	}
+	if(flag==(cnt-1))          
+	{
+		cnt--;
+		cout<<"\nDeleted if(flag==(cnt-1))!\n";
+		
+	}
+	else
+	{
+		for(I=flag;I<cnt;I++)
+		{
+			stdrec[I] = stdrec[I+1];
+		}
+		cnt--;
+		cout<<"\nDeleted !\n";
+	}
+
+    file1.open(indxFilename,ios::out);
+
+	file2.open(filenamechr,ios::out);  
+    if(file2.fail()){
+        cerr<<"Error:"<<strerror(errno);
+    }
+        cout<<"Value of count 1 "<<cnt;  
+    cout<<"Value of count 2"<<cnt;
+
+for(I=0;I<cnt;I++)                   
+	{             
+        if(I==0&&fileName.compare("drug.txt")==0){
+            file2<<"ind|Drug name|Drug type|Department|Stage|Trial-Stage\n";
+        }else if(I==0){
+             file2<<"ind|HSI|HSN|phone|LD|Trial-Stage\n";
+        }  else{
+            file1<<stdrec[I].field1<<"|"<<I-1<<"\n";
+		file2<<I-1<<"|"<<stdrec[I].field1 <<"|"<<stdrec[I].field2<<"|"<<stdrec[I].field3<<"|"<<stdrec[I].field4<<"|"<<stdrec[I].field5<<"\n";
+     
+        }                   
+		
+	}
+
+	file1.close();
+	file2.close();
+	return;
 }
 
-void display_records()
+
+void delete_record(string fileName,string indxFileName)
 {
+
+    int I,flag;
+char idx_no[5],drg_name[5],idx_drugName[5];
+fstream file1;
+
+     if(dashboardFlag==0)
+	cout<<"\nEnter the name of the drug which is to be deleted\n";
+    else
+    cout<<"\nEnter the Human Subject Id, whose record is to be deleted\n";
+
+	cin>>drg_name;
+	file1.open(indxFileName,ios::in);
+	if(!file1)
+	{
+		cout<<"\nError !\n";
+		exit(0);
+	}
+	flag=0;
+
+	while(!file1.eof())
+	{
+		file1.getline(idx_drugName,10,'|');     
+		file1.getline(idx_no,5,'\n');       
+
+		if(strcmp(drg_name,idx_drugName)==0)
+		{
+			delete_drugRec(fileName,indxFileName,idx_no,drg_name); 
+			flag=1;
+			break;
+		}
+	}
+	if(!flag)
+	cout<<"\nDeletion failed !\n"; 
+	file1.close();
+	
+	
+}
+
+void modify(string filename)
+{
+fstream in;
+char buffer[45];
+string drugName,HumanSubId;
+int I,j;
+char filenameChr[15];
+strcpy(filenameChr,filename.c_str());
+int countOfRecs=get_num_records(filenameChr);
+ctm recsObj[countOfRecs];
+
+in.open(filenameChr,ios::in);
+
+if(!in)
+{
+cout<<"\nCannot open the file";
+return;
+}
+ if(dashboardFlag==0)
+cout<<"\nEnter the Drug name\n";
+else 
+cout<<"\nEnter the Human Subject id\n";
+cin>>drugName;
+
+I=0;
+
+while(!in.eof())
+  {
+in.getline(recsObj[I].ind,5,'|');
+in.getline(recsObj[I].field1,20,'|');
+in.getline(recsObj[I].field2,15,'|');
+in.getline(recsObj[I].field3,15,'|');
+in.getline(recsObj[I].field4,10,'|');
+in.getline(recsObj[I].field5,15,'\n');
+cout<<recsObj[I].field1<<" "<<recsObj[I].field2<<" "<<recsObj[I].field3<<" "<<recsObj[I].field4<<" "<<recsObj[I].field5<<"\n";
+I++;
+ }
+
+I--;
+char drugNameChr[20];
+strcpy(drugNameChr,drugName.c_str());
+cout<<"I count: "<<I;
+for(j=0;j<I;j++)
+{
+if(strcmp(drugNameChr,recsObj[j].field1)==0)
+{
+cout<<"\nEnter the new values \n";
+  //cin>>recsObj[j].field1;     //field1 cannot be modified as it is either the drug name or the human subject id
+ cin>>recsObj[j].field2;
+  cin>>recsObj[j].field3;
+ cin>>recsObj[j].field4;
+  cin>>recsObj[j].field5;
+  cout<<"new vals "<<recsObj[j].field1<<" "<<recsObj[j].field2<<" "<<recsObj[j].field3<<" "<<recsObj[j].field4<<" "<<recsObj[j].field5<<"\n";
+break;
+
+  }
+ }
+
+if(j==I)
+ {
+cout<<"\nRecord not found.\n"; 
+return;
+ }
+
+in.close();
+fstream file;
+file.open(filenameChr,ios::out);
+if(!file)
+ {
+cout<<"\nUnable"; 
+return;
+ }
+
+for(j=0;j<I;j++)
+ {
+strcpy(buffer,recsObj[j].ind);
+strcat(buffer,"|");
+strcat(buffer,recsObj[j].field1);
+strcat(buffer,"|");
+strcat(buffer,recsObj[j].field2);
+strcat(buffer,"|");
+strcat(buffer,recsObj[j].field3);
+strcat(buffer,"|");
+strcat(buffer,recsObj[j].field4);
+strcat(buffer,"|");
+strcat(buffer,recsObj[j].field5);
+strcat(buffer,"\n");
+file<<buffer;
+ }
+file.close();
+}
+void display_records(string filename)
+{
+
+    fstream file2;
+     ctm ctmObj;
+
+	
+	file2.open(filename,ios::in);
+    if(dashboardFlag==0)
+cout<<"\n\n"<<"Drugs details : \n";
+    else
+    cout<<"\n\n"<<"Human Subject details : \n";
+	while(!file2.fail())
+ 	{
+		file2.getline(ctmObj.ind,5,'|');
+		file2.getline(ctmObj.field1,20,'|');
+		file2.getline(ctmObj.field2,15,'|');
+		file2.getline(ctmObj.field3,15,'|');
+		file2.getline(ctmObj.field4,10,'|');
+		file2.getline(ctmObj.field5,15,'\n');
+cout<<ctmObj.ind<<"\t\t"<<ctmObj.field1<<"\t\t"<<ctmObj.field2<<"\t\t"<<ctmObj.field3<<"\t\t"<<ctmObj.field4<<"\t\t"<<ctmObj.field5<<"\n";
+	 	
+ 	}
+	file2.close();
 }
 
 int login_page()
@@ -229,7 +471,11 @@ int main()
     std::cout << "\n\t\t\t \t |_______________________________________________________________________________________________________| \n";
 
     int check;
-    check = login_page();
+    string drugFileName;
+    string drugIdxFile;
+    fstream drugFileObj;
+    fstream  indx;
+    check = 1;//login_page();
     if (check == 1)
     {
         cout << "---------------------------------------------------------\n"
@@ -243,14 +489,20 @@ int main()
         {
             std::cout << "PLEASE ENTER YOUR CHOICE:\n";
             cin >> choice;
+            if(choice==1){
+                dashboardFlag=0;
+            }else{
+                dashboardFlag=1;
+            }
             switch (choice)
             {
             case 1:
+            {
                 for (;;)
                 {
                     cout << "\nPlease choose :\n 0:Exit\n 1:Add Drug Record\n";
                     cout << "2:Search Drug Record\n 3:Delete Drug Record\n";
-                    cout << "4:Display Drug Records\n";
+                    cout << "4:Display Drug Records\n" "5.Modify Records\n";
                     cin >> choice;
                     switch (choice)
                     {
@@ -262,27 +514,38 @@ int main()
                     case 1:
                     {
 
-                        string fileName = "drug";
+                        string fileName = "drug.txt";
+                        string indxFilename="drug-idx.txt";
                         cout << "\nEnter the details :\n";
-                        add_record(fileName);
+                        add_record(fileName,indxFilename);
                         break;
                     }
                     case 2:
                     {
-                        string fileName = "drug";
-                        search_record(fileName);
+                        string fileName = "drug.txt";
+                        string indxFilename="drug-idx.txt";
+                        search_record(fileName,indxFilename);
                         break;
                     }
                     case 3:
                     {
-
-                        delete_record();
+                         string fileName = "drug.txt";
+                        string indxFilename="drug-idx.txt";
+                        delete_record(fileName,indxFilename);
                         break;
                     }
                     case 4:
                     {
+                        string fileName = "drug.txt";
 
-                        display_records();
+                        display_records(fileName);
+                        break;
+                    }
+                    case 5:
+                    {
+                        string fileName = "drug.txt";
+
+                        modify(fileName);
                         break;
                     }
                     default:
@@ -293,21 +556,84 @@ int main()
                     }
                 }
                 break;
+            }
 
             case 2:
+            {
                 cout << "\nHUMAN SUBJECTS DASHBOARD\n";
 
-                break;
+                for(;;){
+                    cout<<"Enter the drug name\n";
+                    cin>>drugFileName;
+                    drugIdxFile+=drugFileName+"-idx.txt";
+                  //  drugIdxFile="drugFiles/"+drugIdxFile;
+                    drugFileName+=".txt";
+                   // drugFileName="drugFiles/"+drugFileName;
+                    drugFileObj.open(drugFileName,ios::in|ios::app);
+                    indx.open(drugIdxFile,ios::in|ios::app);
+                    for(;;){
 
-            default:
-                cout << "\nInvalid choice !\n";
-                exit(0);
-            }
-        }
-    }
-
+                    cout << "\nPlease choose :\n 0:Exit\n 1:Add patient Record\n";
+                    cout << "2:Search Drug Record\n 3:Delete Drug Record\n";
+                    cout << "4:Display Drug Records\n" "5.Modify Records\n";
+                    cin >> choice;
+                    switch (choice)
+                    {
+                    case 0:
+                    {
+                        exit(0);
+                        break;
+                    }
+                    case 1:
+                    {
+                        add_record(drugFileName,drugIdxFile);
+                        break;
+                    }
+                    case 2:
+                    {
+                       search_record(drugFileName,drugIdxFile);
+                        break;
+                    }
+                    case 3:
+                    {
+                        delete_record(drugFileName,drugIdxFile);
+                        break;
+                    }
+                    case 4:
+                    {
+                        display_records(drugFileName);
+                        break;
+                    }
+                    case 5:
+                    {
+                        modify(drugFileName);
+                        break;
+                    }
+                    default:{
+                            cout << "\nInvalid choice !\n";
+                            exit(0);
+                            }
+                    }
+                }
+           
+                }
+         break;
+            } 
+        }//end of switch main
+     } //end of for loop main
+    } // end of if
     else
     {
         cout << "Invalid Credentials";
     }
 }
+
+/*ind|HSI|HSN|Phone|LD|Trial-Stage
+0|a|a|a|a|a
+1|b|b|b|b|b
+2|c|c|c|c|c
+
+a|0
+b|1
+c|2
+*/
